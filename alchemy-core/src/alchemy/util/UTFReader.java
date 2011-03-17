d71f5fc211ca05508ce1b39acc32a39176583c32
sbasalaev <sbasalaev@2c98f5b3-ddac-29b7-7766-ceeede3b9166>
2011-03-17 09:13:24 +0000

    Added IO utilities
    git-svn-id: https://alchemy-os.googlecode.com/svn/trunk@5 2c98f5b3-ddac-29b7-7766-ceeede3b9166

---
+++ alchemy-core/src/alchemy/util/UTFReader.java
@@ -0,0 +1,110 @@
+/*
+ * This file is a part of Alchemy OS project.
+ *  Copyright (C) 2011  Sergey Basalaev <sbasalaev@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package alchemy.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.UTFDataFormatException;
+
+/**
+ * Reader of UTF-encoded data.
+ * Also this reader allows one to read a single line of text
+ * and to get a number of the current line.
+ *
+ * @author Sergey Basalaev
+ */
+public class UTFReader extends Reader {
+
+	private InputStream input;
+	private int linenum;
+
+	public UTFReader(InputStream s) {
+		input = s;
+	}
+
+	public synchronized int read() throws IOException {
+		if (input == null) throw new IOException("Stream is closed");
+		int b1 = input.read();
+		if (b1 < 0) return -1;
+		if (b1 <= 0x7f) {
+			if (b1 == '\n') linenum++;
+			return b1;
+		}
+		if (b1 < 0xC0) throw new UTFDataFormatException("Mailformed UTF");
+		int b2 = input.read();
+		if (b2 < 0) throw new UTFDataFormatException("Unended UTF char at EOF");
+		if ((b2 & 0xC0) != 0x80) throw new UTFDataFormatException("Mailformed UTF");
+		if (b1 < 0xE0) {
+			return ((b1 & 0x1f) << 6) | (b2 & 0x3f);
+		}
+		int b3 = input.read();
+		if (b3 < 0) throw new UTFDataFormatException("Unended UTF char at EOF");
+		if ((b3 & 0xC0) != 0x80) throw new UTFDataFormatException("Mailformed UTF");
+		return ((b1 & 0x0f) << 12) | ((b2 & 0x3f) << 6) | (b3 & 0x3f);
+	}
+
+	public synchronized int read(char[] cbuf, int off, int len) throws IOException {
+		if (off+len > cbuf.length || off < 0)
+			throw new IndexOutOfBoundsException();
+		if (len == 0) return 0;
+		int reallen = 0;
+		while (reallen < len) {
+			int ch = read();
+			if (ch < 0) break;
+			cbuf[off+reallen] = (char)ch;
+			reallen++;
+		}
+		if (reallen == 0) return -1;
+		return reallen;
+	}
+
+	public void close() throws IOException {
+		if (input == null) return;
+		input.close();
+		input = null;
+	}
+
+	/**
+	 * Reads a single line of text.
+	 * @return
+	 *   characters until first <code>'\n'</code> or EOF
+	 *   as a string; <code>null</code> if stream is at end
+	 * @throws IOException if an I/O error occurs
+	 */
+	public synchronized String readLine() throws IOException {
+		int ch = read();
+		if (ch < 0) return null;
+		StringBuffer sb = new StringBuffer();
+		while (ch > 0 && ch != '\n') {
+			sb.append((char)ch);
+			ch = read();
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * Returns number of the current line.
+	 * @return current line number
+	 */
+	public int lineNumber() {
+		return linenum;
+	}
+}
---
+++ alchemy-core/src/alchemy/util/Util.java
@@ -0,0 +1,148 @@
+/*
+ * This file is a part of Alchemy OS project.
+ *  Copyright (C) 2011  Sergey Basalaev <sbasalaev@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package alchemy.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UTFDataFormatException;
+
+/**
+ * Misc. utilities used in Alchemy.
+ * @author Sergey Basalaev
+ */
+public final class Util {
+
+	private static final String ERR_END = "Incomplete character at the end";
+	private static final String ERR_WRONG = "Wrong code at byte ";
+	private static final String ERR_LONG = "Encoded string is too long";
+
+	private Util() { }
+
+	/**
+	 * Decodes bytes to String using modified UTF format.
+	 * @param b  byte array
+	 * @return decoded string
+	 * @throws UTFDataFormatException  if given byte sequence is not valid UTF
+	 */
+	public static String utfDecode(byte[] b) throws UTFDataFormatException {
+        int len = b.length;
+		char[] chars = new char[len];
+		int count = 0;                 //count of chars read so far
+		int ofs  = 0;                  //offset in byte array
+		int b1, b2, b3;                //bytes to compound symbols from
+
+		while (count < len) {
+			b1 = b[ofs++] & 0xff;
+			if (b1 < 0x80) {  // 0xxx xxxx
+				chars[count++]=(char)b1;
+				continue;
+			}
+			switch (b1 & 0xf0) {
+				case 0xc0:    // 1100 xxxx   10xx xxxx
+				case 0xd0:    // 1101 xxxx   10xx xxxx
+					if (ofs + 1 > len) throw new UTFDataFormatException(ERR_END);
+					b2 = b[ofs++];
+					if ((b2 & 0xc0) != 0x80) throw new UTFDataFormatException(ERR_WRONG+(ofs-1));
+					chars[count++] = (char)( ((b1 & 0x1f) << 6) | (b2 & 0x3f) );
+					break;
+				case 0xe0:    // 1110 xxxx   10xx xxxx   10xx xxxx
+					if (ofs + 2 > len) throw new UTFDataFormatException(ERR_END);
+					b2 = b[ofs++];
+					b3 = b[ofs++];
+					if (((b2 & 0xc0) != 0x80)) throw new UTFDataFormatException(ERR_WRONG+(ofs-2));
+					if (((b3 & 0xc0) != 0x80)) throw new UTFDataFormatException(ERR_WRONG+(ofs-1));
+					chars[count++] = (char)( ((b1 & 0x0f) << 12) | ((b2 & 0x3f) << 6) | (b3 & 0x3f) );
+					break;
+				default:
+					throw new UTFDataFormatException(ERR_WRONG+(ofs-1));
+			}
+		}
+
+        return new String(chars, 0, count);
+	}
+
+	/**
+	 * Encodes string in byte array in modified UTF format.
+	 * @param str  string to encode
+	 * @return  byte array containing encoded string
+	 * @throws UTFDataFormatException if string is too long to encode
+	 */
+	public static byte[] utfEncode(String str) throws UTFDataFormatException {
+		int len = str.length();  //string length, C.O.
+		int count = 0;           //a count of bytes
+		char ch;                 //current character
+		byte[] bytes;            //result of encoding
+		int ofs = 0;             //offset in byte array
+
+		//calculating byte count
+		for (int i=0; i<len; i++) {
+			ch = str.charAt(i);
+			if (ch < 0x80) count++;
+			else if (ch < 0x0800) count += 2;
+			else count += 3;
+		}
+
+		if (count >= 0xffff)
+			throw new UTFDataFormatException(ERR_LONG);
+
+		bytes = new byte[count];
+
+		//string encoding
+		for (int i=0; i<len; i++) {
+			ch = str.charAt(i);
+			if (ch < 0x80) {
+				bytes[ofs++] = (byte)(ch);
+			} else if (ch < 0x800) {
+				bytes[ofs++] = (byte)(ch >> 6);
+				bytes[ofs++] = (byte)(ch);
+			} else {
+				bytes[ofs++] = (byte)(ch >> 12);
+				bytes[ofs++] = (byte)(ch >> 6);
+				bytes[ofs++] = (byte)(ch);
+			}
+		}
+
+		return bytes;
+	}
+
+	/**
+	 * Reads until the end of the stream and returns result as a byte array.
+	 * @param s input stream
+	 * @return byte array containing all bytes read
+	 * @throws IOException if an I/O error occurs
+	 */
+	public static byte[] readFully(InputStream s) throws IOException {
+		int count = 0;
+		byte[] buf = new byte[32];
+		while (true) {
+			if (count == buf.length) {
+				byte[] newbuf = new byte[count << 1];
+				System.arraycopy(buf, 0, newbuf, 0, count);
+				buf = newbuf;
+			}
+			int len = s.read(buf, count, buf.length-count);
+			if (len <= 0) break;
+			count += len;
+		}
+		byte[] ret = new byte[count];
+		System.arraycopy(buf, 0, ret, 0, count);
+		return ret;
+	}
+}
