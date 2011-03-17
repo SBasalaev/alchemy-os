b214d0cc2f733a29c064556176978cd3efc75183
sbasalaev <sbasalaev@2c98f5b3-ddac-29b7-7766-ceeede3b9166>
2011-03-17 08:24:43 +0000

    Project for core components. Documented File and Filesystem classes.
    git-svn-id: https://alchemy-os.googlecode.com/svn/trunk@3 2c98f5b3-ddac-29b7-7766-ceeede3b9166

---
+++ alchemy-core/src/alchemy/fs/File.java
@@ -0,0 +1,190 @@
+/*
+ * This file is a part of Alchemy OS project.
+ *  Copyright (C) 2011  Sergey Basalaev <sbasalaev@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package alchemy.fs;
+
+/**
+ * Represents path to the file in the directory tree.
+ * Path is a sequence of filenames each starting with slash '/'
+ * (e.g. "/path/to/the/file"). Path for the root directory
+ * is represented by empty string.
+ * <p/>
+ * While constructing <code>File</code> object the path is normalized:
+ * <ol>
+ *   <li>
+ *     All sequences of slashes are replaced with a single slash.
+ *   </li>
+ *   <li>
+ *     Every occurence of <code>'/.'</code> is removed from the sequence.
+ *   </li>
+ *   <li>
+ *     Every occurence of <code>'/<i>name</i>/..'</code> is removed
+ *     from the sequence.
+ *   </li>
+ *   <li>
+ *     All leading <code>'/..'</code> are removed from the sequence.
+ *   </li>
+ * </ol>
+ * Normalized path is unique for each file. It can be obtained
+ * using {@link #path()} method.
+ *
+ * @author Sergey Basalaev
+ */
+public final class File {
+
+	private final String path;
+
+	/**
+	 * Creates new <code>File</code> for the specified path.
+	 * @param  path  path to the file
+	 * @throws IllegalArgumentException
+	 *   if parameter is not a valid path
+	 * @throws NullPointerException
+	 *   if parameter is <code>null</code>
+	 */
+	public File(String path) {
+		this.path = normalize(path);
+	}
+
+	/**
+	 * Creates new <code>File</code> for the specified parent
+	 * directory and a path relative to it.
+	 * @param parent   parent file
+	 * @param relpath  path relative to the parent file
+	 */
+	public File(File parent, String relpath) {
+		this.path = normalize(parent.path+'/'+relpath);
+	}
+
+	/**
+	 * Validates and normalizes path.
+	 * Valid path either empty or starts with slash and not ends with
+	 * slash. Normalization includes:
+	 * <ol>
+	 *   <li>
+	 *     Replacing all sequences of slashes with single slash.
+	 *   </li>
+	 *   <li>
+	 *     Removing every occurence of <code>'/.'</code>.
+	 *   </li>
+	 *   <li>
+	 *     Removing every occurence of <code>'/<i>name</i>/..'</code>.
+	 *   </li>
+	 * </ol>
+	 *
+	 * @param path  path to the file
+	 *
+	 * @throws IllegalArgumentException if parameter is not valid path
+	 * @throws NullPointerException if parameter is <code>null</code>
+	 * @return normalized path
+	 */
+	private static String normalize(String path) {
+		if (path.length() == 0) return path;
+		if ((path.charAt(0) != '/') || (path.charAt(path.length()-1) == '/'))
+			throw new IllegalArgumentException(path);
+		//we know string starts with '/' and not ends with '/'
+		StringBuffer sb = new StringBuffer();
+		while (path.length() > 0) {
+			int start = 1;
+			while (path.charAt(start) == '/') start++;
+			int end = path.indexOf('/', start);
+			if (end < 0) end = path.length();
+			start--;
+			String next = path.substring(start, end);
+			path = path.substring(end);
+			if (next.equals("/.")) {
+				//do nothing with it
+			} else if (next.equals("/..")) {
+				int len = sb.length();
+				if (len > 0) {
+					len--;
+					while (sb.charAt(len) != '/') len--;
+					sb.delete(len, sb.length());
+				}
+			} else {
+				sb.append(next);
+			}
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * Returns a parent for this file.
+	 * For root directory this method returns <code>null</code>.
+	 *
+	 * @return a parent for this file or <code>null</code>
+	 *         if this file have no parent
+	 */
+	public File parent() {
+		if (path.length() == 0) return null;
+		return new File(path.substring(0, path.lastIndexOf('/')));
+	}
+
+	/**
+	 * Returns path to this file.
+	 * @return path to this file
+	 */
+	public String path() {
+		return path;
+	}
+
+	/**
+	 * Returns the name of this file.
+	 * For root directory this method returns empty string.
+	 *
+	 * @return a file name
+	 */
+	public String name() {
+		return path.substring(path.lastIndexOf('/')+1);
+	}
+
+	/**
+	 * Tests whether this object equals to another one.
+	 *
+	 * @param obj another object
+	 * @return <code>true</code> if objects are the same,
+	 *         <code>false</code> otherwise
+	 */
+	public boolean equals(Object obj) {
+		if (obj == null) return false;
+		if (getClass() != obj.getClass()) return false;
+		final File other = (File) obj;
+		return this.path.equals(other.path);
+	}
+
+	/**
+	 * Returns a hash code for this object.
+	 *
+	 * @return a hash code for the object
+	 */
+	public int hashCode() {
+		return 413 ^ this.path.hashCode();
+	}
+
+	/**
+	 * Returns a string representation of this object.
+	 * A file path is returned.
+	 *
+	 * @return a string representation of the object
+	 */
+	public String toString() {
+		return path;
+	}
+
+}
---
+++ alchemy-core/src/alchemy/fs/Filesystem.java
@@ -0,0 +1,364 @@
+/*
+ * This file is a part of Alchemy OS project.
+ *  Copyright (C) 2011  Sergey Basalaev <sbasalaev@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package alchemy.fs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Filesystem abstraction.
+ *
+ * @see File
+ *
+ * @author Sergey Basalaev
+ */
+public abstract class Filesystem {
+
+	/** Constructor for subclasses. */
+	protected Filesystem() { }
+
+	/**
+	 * Returns a stream to read from the file.
+	 *
+	 * @param file a file to read from
+	 * @return <code>InputStream</code> instance
+	 * @throws IOException
+	 *   if file does not exist, is not accessible,
+	 *   is a directory or I/O error occurs
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 */
+	public abstract InputStream read(File file) throws IOException;
+
+	/**
+	 * Returns a stream to write to the file.
+	 * Contents of the file becomes overwritten.
+	 * If file does not exist it is created.
+	 *
+	 * @param file a file to write to
+	 * @return <code>OutputStream</code> instance
+	 * @throws IOException
+	 *   if file cannot be created, is not accessible,
+	 *   is a directory or I/O error occurs
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @see #append(File)
+	 */
+	public abstract OutputStream write(File file) throws IOException;
+
+	/**
+	 * Returns a stream to write to the file.
+	 * New contents is added to the end of file.
+	 * If file does not exist it is created.
+	 *
+	 * @param file a file to write to
+	 * @return <code>OutputStream</code> instance
+	 * @throws IOException
+	 *   if file cannot be created, is not accessible,
+	 *   is a directory or I/O error occurs
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @see #write(File)
+	 */
+	public abstract OutputStream append(File file) throws IOException;
+
+	/**
+	 * Lists file names that the specified directory contains.
+	 * The pathnames ".." and "." are not included in the
+	 * output. If the directory is empty then an empty
+	 * array is returned.
+	 * 
+	 * @param file a directory to list
+	 * @return
+	 *   array of file names the specified director contains
+	 * @throws IOException
+	 *   if file does not exist, cannot be accessed,
+	 *   is not a directory or I/O error occurs
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 */
+	public abstract String[] list(File file) throws IOException;
+
+	/**
+	 * Tests whether the specified file exists.
+	 * If an I/O error occurs then method
+	 * returns <code>false</code>.
+	 *
+	 * @param file a file to test
+	 * @return <code>true</code> if file exists,
+	 *         <code>false</code> otherwise
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 */
+	public abstract boolean exists(File file);
+
+	/**
+	 * Tests whether the specified file exists and a directory.
+	 *
+	 * @param file a file to test
+	 * @return <code>true</code> if file exists and a directory,
+	 *         <code>false</code> otherwise
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @see #exists(File)
+	 */
+	public abstract boolean isDirectory(File file);
+
+	/**
+	 * Creates new file in a filesystem.
+	 *
+	 * @param file a file to create
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file exists, cannot be accessed or I/O error occurs
+	 */
+	public abstract void create(File file) throws IOException;
+
+	/**
+	 * Creates new directory in a filesystem.
+	 *
+	 * @param file a directory to create
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file exists, cannot be accessed or I/O error occurs
+	 */
+	public abstract void mkdir(File file) throws IOException;
+
+	/**
+	 * Removes file from the system.
+	 *
+	 * @param file a file to remove
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file cannot be accessed, is non-empty
+	 *   directory or I/O error occurs
+	 */
+	public abstract void remove(File file) throws IOException;
+
+	/**
+	 * Copies contents of one file to another.
+	 * <p/>
+	 * Default implementation opens <code>source</code>
+	 * for reading, <code>dest</code> for writing and
+	 * writes contents of the first file to the second.
+	 * Subclasses are encouraged to override this method
+	 * if file system supports more efficient method of
+	 * copying files.
+	 *
+	 * @param source  the origin of copying
+	 * @param dest    the destination of copying
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if I/O error occurs during copying
+	 */
+	public void copy(File source, File dest) throws IOException {
+		InputStream in = read(source);
+		OutputStream out = write(dest);
+		try {
+			byte[] buf = new byte[128];
+			int count = in.read(buf);
+			while (count >= 0) {
+				out.write(buf, 0, count);
+				count = in.read(buf);
+			}
+			out.flush();
+		} finally {
+			try {
+				in.close();
+			} catch (IOException ioe) { }
+			try {
+				out.close();
+			} catch (IOException ioe) { }
+		}
+	}
+
+	/**
+	 * Moves file to a new location.
+	 * <p/>
+	 * The default implementation copies <code>source</code>
+	 * to <code>dest</code> and then removes <code>source</code>
+	 * as if methods
+	 * <pre>
+	 * copy(source, dest);
+	 * remove(source);</pre>
+	 * were called.
+	 * Subclasses are encouraged to override this method
+	 * if file system supports more efficient method of
+	 * moving files.
+	 *
+	 * @param source  the original file
+	 * @param dest    new name of the file
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if an I/O error occurs during moving
+	 */
+	public void move(File source, File dest) throws IOException {
+		copy(source, dest);
+		remove(source);
+	}
+
+	/**
+	 * Returns the time of the last modification of the file.
+	 *
+	 * @param file the file
+	 * @return time in format used by <code>System.currentTimeMillis()</code>
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file does not exist, cannot be accessed
+	 *   or I/O error occurs
+	 * @see System#currentTimeMillis()
+	 */
+	public abstract long lastModified(File file) throws IOException;
+
+	/**
+	 * Tests whether this file exists and can be read.
+	 * If an I/O error occurs this method returns <code>false</code>.
+	 *
+	 * @param file the file
+	 * @return <code>true</code> if this file can be read,
+	 *         <code>false</code> otherwise
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 */
+	public abstract boolean canRead(File file);
+
+	/**
+	 * Tests whether this file can be written.
+	 * If an I/O error occurs this method returns <code>false</code>.
+	 *
+	 * @param file the file
+	 * @return <code>true</code> if this file can be written,
+	 *         <code>false</code> otherwise
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 */
+	public abstract boolean canWrite(File file);
+
+	/**
+	 * Tests whether this file can be executed.
+	 * If an I/O error occurs this method returns <code>false</code>.
+	 *
+	 * @param file the file
+	 * @return <code>true</code> if this file can be executed,
+	 *         <code>false</code> otherwise
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 */
+	public abstract boolean canExec(File file);
+
+	/**
+	 * Changes ability of file to be read.
+	 *
+	 * @param file  the file
+	 * @param on    if <code>true</code>, file can be read;
+	 *              if <code>false</code>, it cannot
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file does not exist, cannot be accessed
+	 *   or I/O error occurs
+	 */
+	public abstract void setRead(File file, boolean on) throws IOException;
+
+	/**
+	 * Changes ability of file to be written.
+	 * @param file  the file
+	 * @param on    if <code>true</code>, file can be written;
+	 *              if <code>false</code>, it cannot
+	 * 
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file does not exist, cannot be accessed
+	 *   or I/O error occurs
+	 */
+	public abstract void setWrite(File file, boolean on) throws IOException;
+
+	/**
+	 * Changes ability of file to be executed.
+	 *
+	 * @param file  the file
+	 * @param on    if <code>true</code>, file can be executed;
+	 *              if <code>false</code>, it cannot
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file does not exist, cannot be accessed
+	 *   or I/O error occurs
+	 */
+	public abstract void setExec(File file, boolean on) throws IOException;
+
+	/**
+	 * Returns the size of the file.
+	 * If file is accessible but the size cannot be computed
+	 * this method returns <code>-1</code>.
+	 * 
+	 * @param file the file
+	 * @return file size, in bytes
+
+	 * @throws SecurityException
+	 *   if application is not granted access to the file
+	 * @throws IOException
+	 *   if file does not exist, cannot be accessed
+	 *   or I/O error occurs
+	 */
+	public abstract int size(File file) throws IOException;
+
+	/**
+	 * Determines the total size of this filesystem.
+	 *
+	 * @return
+	 *   the total size of the file system in bytes, or
+	 *   <code>-1</code> if this value cannot be estimated
+	 * @throws SecurityException
+	 *   if application is not granted access to the file system
+	 */
+	public abstract long spaceTotal();
+
+	/**
+	 * Determines the free memory that is available on the
+	 * file system.
+	 *
+	 * @return
+	 *  the available size in bytes on a file system, or
+	 *   <code>-1</code> if this value cannot be estimated
+	 * @throws SecurityException
+	 *   if application is not granted access to the file system
+	 */
+	public abstract long spaceFree();
+
+	/**
+	 * Determines the used memory of the file system.
+	 *
+	 * @return
+	 *  the used size in bytes on a file system, or
+	 *   <code>-1</code> if this value cannot be estimated
+	 * @throws SecurityException
+	 *   if application is not granted access to the file system
+	 */
+	public abstract long spaceUsed();
+}
