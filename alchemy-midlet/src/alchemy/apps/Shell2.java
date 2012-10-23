de3738dc51f5a1682e08243a8602fa7e63d11f49
Sergey Basalaev <sbasalaev@gmail.com>
2012-10-24 01:54:26 +0700

    Comment out unimplemented stuff

--- alchemy-midlet/src/alchemy/apps/Shell2.java
+++ alchemy-midlet/src/alchemy/apps/Shell2.java
@@ -1,19 +1,22 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package alchemy.apps;
+// I failed to find time to write new shell for the release 2.0
+// It will be implemented in the next major release
 
-import alchemy.core.Context;
-import alchemy.nlib.NativeApp;
-
-/**
- * Native shell for Alchemy OS 2.0
- * @author Sergey Basalaev
- */
-public class Shell2 extends NativeApp {
-
-	public int main(Context c, String[] args) {
-		return 0;
-	}
-}
+///*
+// * To change this template, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package alchemy.apps;
+//
+//import alchemy.core.Context;
+//import alchemy.nlib.NativeApp;
+//
+///**
+// * Native shell for Alchemy OS 2.0
+// * @author Sergey Basalaev
+// */
+//public class Shell2 extends NativeApp {
+//
+//	public int main(Context c, String[] args) {
+//		return 0;
+//	}
+//}
