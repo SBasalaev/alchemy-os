19e5ddface140755683ce2f38184deb2cbcbde0f
Sergey Basalaev <sbasalaev@gmail.com>
2013-01-30 16:58:34 +0700

    Add Bool.tostr and builtin header to libcore

--- libs/libcore/install.sh
+++ libs/libcore/install.sh
@@ -2,4 +2,4 @@
 
 install native/libcore.3.0.so native/libcore.3.so native/libcore.so /lib
 install libcoree.3.0.so libcoree.3.so libcoree.so /lib
-install connection.eh dataio.eh dict.eh dl.eh error.eh func.eh io.eh list.eh math.eh rnd.eh strbuf.eh string.eh sys.eh textio.eh time.eh /inc
+install builtin.eh connection.eh dataio.eh dict.eh dl.eh error.eh func.eh io.eh list.eh math.eh rnd.eh strbuf.eh string.eh sys.eh textio.eh time.eh /inc
