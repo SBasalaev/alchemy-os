70c12deb5343a8744f5d55cbc12329ce7b0216c3
Sergey Basalaev <sbasalaev@gmail.com>
2013-02-06 00:09:15 +0700

    libcore3 - update build scripts

--- libs/libcore/clean.sh
+++ libs/libcore/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rm -f dataio.o io.o list.o math.o strbuf.o sys.o textio.o libcoree.3.0.so libcoree.3.so libcoree.so
+rm -f dataio.o io.o list.o math.o strbuf.o sys.o textio.o libcoree.3.1.so libcoree.3.0.so libcoree.3.so libcoree.so
--- libs/libcore/make.sh
+++ libs/libcore/make.sh
@@ -7,8 +7,10 @@ ec math.e -o math.o
 ec strbuf.e -o strbuf.o
 ec textio.e -o textio.o
 ec sys.e -o sys.o
-env "LIBPATH=native/" el dataio.o io.o list.o math.o strbuf.o sys.o textio.o -o libcoree.3.0.so -slibcoree.3.so
-echo '#=libcoree.3.0.so' > libcoree.3.so
-echo '#=libcoree.3.0.so' > libcoree.so
+env "LIBPATH=native/" el dataio.o io.o list.o math.o strbuf.o sys.o textio.o -o libcoree.3.1.so -slibcoree.3.so
+echo '#=libcoree.3.1.so' > libcoree.3.0.so
+echo '#=libcoree.3.1.so' > libcoree.3.so
+echo '#=libcoree.3.1.so' > libcoree.so
+chmod +x libcoree.3.0.so
 chmod +x libcoree.3.so
 chmod +x libcoree.so
