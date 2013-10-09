a6f811429e9fdba05d7f76b2932e7039db1ab501
Sergey Basalaev <sbasalaev@gmail.com>
2013-10-09 21:51:02 +0700

    Remove deprecated files

--- libs/corelibs/libcore/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-install native/libcore.3.1.so native/libcore.3.0.so native/libcore.3.so native/libcore.so /lib
-install libcoree.3.1.so libcoree.3.0.so libcoree.3.so libcoree.so /lib
-install builtin.eh connection.eh dataio.eh dict.eh dl.eh error.eh func.eh i18n.eh io.eh list.eh math.eh pipe.eh rnd.eh strbuf.eh string.eh sys.eh textio.eh time.eh /inc
