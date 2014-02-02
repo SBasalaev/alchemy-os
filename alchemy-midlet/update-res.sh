a09359b07db90cb03529e112d18a081bad61f369
Sergey Basalaev <sbasalaev@gmail.com>
2014-02-03 01:21:14 +0700

    Remove obsolete installscripts

--- alchemy-midlet/update-res.sh
+++ alchemy-midlet/update-res.sh
@@ -1,13 +1,5 @@
 #!/bin/sh 
 
-echo 'Updating installscripts...'
-cd ../installscripts/install
-# Create empty directories in case git got rid of them
-mkdir -p bin lib cfg res dev tmp home cfg/pkg/db/sources cfg/pkg/db/lists
-arh c install.arh *
-cd -
-mv ../installscripts/install/install.arh res/
-
 echo 'Updating library symbols...'
 cp ../libs/libcore/libcore31.symbols res/
 cp ../libs/libui/libui1.symbols res/
