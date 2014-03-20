92a76d2b0f980d2762ad4185a8fcb37167670fec
Sergey Basalaev <sbasalaev@gmail.com>
2014-03-20 17:32:20 +0700

    setting to null in hashmap acts as remove()

--- alchemy-core/src/alchemy/util/HashMap.java
+++ alchemy-core/src/alchemy/util/HashMap.java
@@ -52,9 +52,11 @@ public final class HashMap {
 
 	/** Sets new value to given key. */
 	public void set(Object key, Object value) {
-		if (value == null)
-			throw new NullPointerException();
-		
+		if (value == null) {
+			this.remove(key);
+			return;
+		}
+
 		// search existing entry
 		HashMapEntry[] table = entries;
 		int hash = key.hashCode();
